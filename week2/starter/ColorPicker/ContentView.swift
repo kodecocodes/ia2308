b23bc5bc2f2b06baf05aa5f6df091dcd64b38683
Javier Gomez <javiergomezdev@gmail.com>
2023-09-10 08:55:37 -0500

    Programming Assignment 02
    done the basics

--- week2/starter/ColorPicker/ContentView.swift
+++ week2/starter/ColorPicker/ContentView.swift
@@ -1,54 +1,50 @@
 import SwiftUI
 
 struct ContentView: View {
+  @Environment(\.colorScheme) var colorScheme
   @State private var alertIsVisible: Bool = false
   @State private var redColor: Double = 0.0
   @State private var greenColor: Double = 0.0
   @State private var blueColor: Double = 0.0
   @State private var foregroundColor = Color(red: 0, green: 0, blue: 0)
-
+  
+  
   var body: some View {
-
-    VStack {
-      Text("Color Picker")
-        .font(.largeTitle)
-
-      RoundedRectangle(cornerRadius: 0)
-        .foregroundColor(foregroundColor)
-        .border(.black)
-      VStack {
-        Text("Red")
-        HStack {
-          Slider(value: $redColor, in: 0...255)
-          Text("\(Int(redColor.rounded()))")
+     VStack {
+        Text("Color Picker")
+          .font(.largeTitle)
+          .padding()
+        
+        ColorPreviewView(color: $foregroundColor)
+          .padding()
+        
+        ColorSliderView(colorValue: $redColor, label: "Rojo", accentColor: .red)
+        ColorSliderView(colorValue: $greenColor, label: "Verde", accentColor: .green)
+        ColorSliderView(colorValue: $blueColor, label: "Azul", accentColor: .blue)
+        
+        Button("Set Color") {
+          foregroundColor = Color(red: redColor / Constants.General.maxValue,
+                                  green: greenColor / Constants.General.maxValue,
+                                  blue: blueColor / Constants.General.maxValue)
         }
+        .padding()
+        .background(Color.blue)
+        .foregroundColor(.white)
+        .cornerRadius(10)
+        .overlay(
+          RoundedRectangle(cornerRadius: 10)
+            .stroke(Color.white, lineWidth: 2)
+        )
       }
-      VStack {
-        Text("Green")
-        HStack {
-          Slider(value: $greenColor, in: 0...255)
-          Text("\(Int(greenColor.rounded()))")
-        }
-      }
-      VStack {
-        Text("Blue")
-        HStack {
-          Slider(value: $blueColor, in: 0...255)
-          Text("\(Int(blueColor.rounded()))")
-        }
-      }
-      Button("Set Color") {
-        foregroundColor = Color(red: redColor / 255, green: greenColor / 255, blue: blueColor / 255)
-      }
+      .background(colorScheme == .dark ? Color.black : Color.white)
+      .padding(10)
     }
-    .background(Color.white)
-    .padding(20)
-
-  }
-}
-
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
+    
+  struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+      ContentView()
+      ContentView().preferredColorScheme(.dark)
+      
+    }
   }
 }
