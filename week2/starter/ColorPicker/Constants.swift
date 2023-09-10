c71c29e543b0df2b3cb26aecf413d63dc4bf9c47
Javier Gomez <javiergomezdev@gmail.com>
2023-09-10 13:04:08 -0500

    Programming assignment HW02

---
+++ week2/starter/ColorPicker/Constants.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum Constants {
+    enum General{
+        public static let minValue = CGFloat(0.0)
+        public static let maxValue = CGFloat(255)
+        public static let cornerRadius = CGFloat(10)
+        public static let borderWidth = CGFloat(2)
+        
+    }
+}
+
