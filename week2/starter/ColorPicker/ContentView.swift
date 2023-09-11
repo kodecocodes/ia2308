6b178d68f03b366e742ae582929b2c68a6807f79
Javier Gómez <javiergomezdev@gmail.com>
2023-09-11 10:56:30 -0500

    Part 3 - Above and Beyond.

--- week2/starter/ColorPicker/ContentView.swift
+++ week2/starter/ColorPicker/ContentView.swift
@@ -1,43 +1,20 @@
 import SwiftUI
 
 struct ContentView: View {
-  @Environment(\.colorScheme) var colorScheme
-  @State private var alertIsVisible: Bool = false
-  @State private var redColor: Double = 0.0
-  @State private var greenColor: Double = 0.0
-  @State private var blueColor: Double = 0.0
-  @State private var foregroundColor = Color(red: 0, green: 0, blue: 0)
-  
-  
-  var body: some View {
-     VStack {
-        Text("Color Picker")
-          .font(.largeTitle)
-          .padding()
-        
-        ColorPreviewView(color: $foregroundColor)
-          .padding()
-        
-        ColorSliderView(colorValue: $redColor, label: "Rojo", accentColor: .red)
-        ColorSliderView(colorValue: $greenColor, label: "Verde", accentColor: .green)
-        ColorSliderView(colorValue: $blueColor, label: "Azul", accentColor: .blue)
-        
-        Button("Set Color") {
-          foregroundColor = Color(red: redColor / Constants.General.maxValue,
-                                  green: greenColor / Constants.General.maxValue,
-                                  blue: blueColor / Constants.General.maxValue)
-        }
-        .padding()
-        .background(Color.blue)
-        .foregroundColor(.white)
-        .cornerRadius(10)
-        .overlay(
-          RoundedRectangle(cornerRadius: 10)
-            .stroke(Color.white, lineWidth: 2)
-        )
+       
+       @Environment(\.horizontalSizeClass) var horizontalSizeClass
+       @Environment(\.verticalSizeClass) var verticalSizeClass
+    
+    var body: some View {
+            if horizontalSizeClass == .compact && verticalSizeClass == .regular {
+                // Device is in Portrait mode
+                VertiView()
+            } else {
+                // Device is in Landscape mode
+                HoriView()
+            }
       }
-      .background(colorScheme == .dark ? Color.black : Color.white)
-      .padding(10)
+      
     }
     
   struct ContentView_Previews: PreviewProvider {
@@ -47,4 +24,3 @@ struct ContentView: View {
       
     }
   }
-}
--- week2/starter/ColorPicker/structView.swift
+++ week2/starter/ColorPicker/structView.swift
@@ -30,3 +30,86 @@ struct ColorSliderView: View {
     }
   }
 }
+
+
+struct VertiView: View {
+  @Environment(\.colorScheme) var colorScheme
+  @State private var alertIsVisible: Bool = false
+  @State private var redColor: Double = 0.0
+  @State private var greenColor: Double = 0.0
+  @State private var blueColor: Double = 0.0
+  @State private var foregroundColor = Color(red: 0, green: 0, blue: 0)
+  
+  var body: some View {
+    VStack {
+      Text("Color Picker")
+        .font(.largeTitle)
+        .padding()
+      
+      ColorPreviewView(color: $foregroundColor)
+        .padding()
+      
+      ColorSliderView(colorValue: $redColor, label: "Rojo", accentColor: .red)
+      ColorSliderView(colorValue: $greenColor, label: "Verde", accentColor: .green)
+      ColorSliderView(colorValue: $blueColor, label: "Azul", accentColor: .blue)
+      
+      Button("Set Color") {
+        foregroundColor = Color(red: redColor / Constants.General.maxValue,
+                                green: greenColor / Constants.General.maxValue,
+                                blue: blueColor / Constants.General.maxValue)
+      }
+      .padding()
+      .background(Color.blue)
+      .foregroundColor(.white)
+      .cornerRadius(10)
+      .overlay(
+        RoundedRectangle(cornerRadius: 10)
+          .stroke(Color.white, lineWidth: 2)
+      )
+    }
+    .background(colorScheme == .dark ? Color.black : Color.white)
+    .padding(10)
+  }
+}
+struct HoriView: View {
+  @Environment(\.colorScheme) var colorScheme
+  @State private var alertIsVisible: Bool = false
+  @State private var redColor: Double = 0.0
+  @State private var greenColor: Double = 0.0
+  @State private var blueColor: Double = 0.0
+  @State private var foregroundColor = Color(red: 0, green: 0, blue: 0)
+  
+  var body: some View {
+    HStack {
+      VStack {
+        Text("Color Picker")
+          .font(.largeTitle)
+          .padding()
+        
+        ColorPreviewView(color: $foregroundColor)
+          .padding()
+      }
+      VStack {
+        ColorSliderView(colorValue: $redColor, label: "Rojo", accentColor: .red)
+        ColorSliderView(colorValue: $greenColor, label: "Verde", accentColor: .green)
+        ColorSliderView(colorValue: $blueColor, label: "Azul", accentColor: .blue)
+        
+        Button("Set Color") {
+          foregroundColor = Color(red: redColor / Constants.General.maxValue,
+                                  green: greenColor / Constants.General.maxValue,
+                                  blue: blueColor / Constants.General.maxValue)
+        }
+        .padding()
+        .background(Color.blue)
+        .foregroundColor(.white)
+        .cornerRadius(10)
+        .overlay(
+          RoundedRectangle(cornerRadius: 10)
+            .stroke(Color.white, lineWidth: 2)
+        )
+      }
+    }
+    .background(colorScheme == .dark ? Color.black : Color.white)
+    .padding(10)
+  }
+}
